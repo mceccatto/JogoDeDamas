817593a6d369ff6d065b0a76171195618e341e22
Marcelo Ceccatto <58266548+mceccatto@users.noreply.github.com>
2022-04-28 19:54:38 -0300

    finalizacao

--- src/br/dev/codelabs/consultas/Pontuacao.java
+++ src/br/dev/codelabs/consultas/Pontuacao.java
@@ -8,11 +8,9 @@ public class Pontuacao {
 		int vendecor = 0,pontosA = Construtor.placar.get(0).getPontos(), pontosB = Construtor.placar.get(1).getPontos();
 		if(pontosA == 10 && pontosB == 10 || pontosA == 11 && pontosB == 11) {
 			vendecor = 3;
-		}
-		if(pontosA == 11 && pontosB == 10 || pontosA == 12 || pontosB == 12) {
+		} else if(pontosA == 12 || pontosA == 11 && pontosB <= 10 || pontosA == 12 || pontosB <= 11) {
 			vendecor = 1;
-		}
-		if(pontosA == 10 && pontosB == 11 || pontosA == 12 || pontosB == 12) {
+		} else if(pontosB == 12 || pontosA <= 10 && pontosB == 11 || pontosA <= 11 || pontosB == 12) {
 			vendecor = 2;
 		}
 		return vendecor;
